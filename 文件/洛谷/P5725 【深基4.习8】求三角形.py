e48adbc5284a3c7ec3c99163a5b9a2c535ac7910
wenxin8 <62378308+wenxin8@users.noreply.github.com>
2020-03-26 20:12:24 +0800

    更新

--- 文件/洛谷/P5725 【深基4.习8】求三角形.py
+++ 文件/洛谷/P5725 【深基4.习8】求三角形.py
@@ -1,15 +1,15 @@
 a = int(input())
 s = 1
-for i in range(0, a):
-    for j in range(0, a):
+for i in range(1, a + 1):   # 行
+    for j in range(1, a + 1):   # 列
         print("%02d" % s, end='')
         s = s + 1
     print()
 print()
 s = 1
-for i in range(0, a):
-    for j in range(0, a ):
-        if j < (a -1 - i):
+for i in range(1, a + 1):   # 行
+    for j in range(1, a + 1):   # 列
+        if j <= (a - i):
             print(' ', end = ' ')
         else:
             print("%02d" % s, end='')
